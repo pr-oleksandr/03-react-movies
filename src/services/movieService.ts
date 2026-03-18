be9e4903606b482ebbbdcd4040ed5bcfd8816ea0
Саша Присташ <sashko@MacBook-Air-Sasa.local>
2026-03-18 16:55:05 +0200

    add search functional and modal

---
+++ src/services/movieService.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import type { Movie } from "../types/movie";
+
+const myKey = import.meta.env.VITE_TMDB_TOKEN;
+
+export async function fetchMovies(movieName: string) {
+    const { data } = await axios.get<{ results: Movie[] }>("https://api.themoviedb.org/3/search/movie", {
+        params: {
+            query: movieName,
+        },
+        headers: {
+            Authorization: `Bearer ${myKey}`
+        }
+    })
+
+    return data
+
+}
